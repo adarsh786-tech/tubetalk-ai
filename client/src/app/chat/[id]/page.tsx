85ef55dec0ab48fe73e98c142a602a1663fba4c5
mihir <mihir.1si17cs058@gmail.com>
2025-05-01 21:25:57 +0530

    Fix

--- client/src/app/chat/[id]/page.tsx
+++ client/src/app/chat/[id]/page.tsx
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 import ChatPage from "@/components/ChatPage";
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
 const ChatBot = async ({ params }: { params: { id: any } }) => {
-  const id = (await params).id;
+  const id = params.id;
   return <ChatPage videoId={id} />;
 };
 
